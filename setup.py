ceea8ef5e6b0c5393fb23400a5b6f37195179d85
kyle@canonical.com <kyrofa@localhost.localdomain>
2017-11-29 01:20:40 +0000

    Progress.

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name='rpi-bot',
+    version='0.0.1',
+    author='Kyle Fazzari',
+    author_email='kyrofa@ubuntu.com',
+    description='Drive a robot with python',
+    scripts=['wheels']
+)
